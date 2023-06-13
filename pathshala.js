b4d33c8eec9fe05597bcf91416d830592f67c946
Bhavya S Patel <102239817+Bhavu2412@users.noreply.github.com>
2023-06-13 19:58:30 +0530

    Add files via upload

---
+++ pathshala.js
@@ -0,0 +1,20 @@
+let home = document.getElementById('home');
+let au = document.getElementById('aboutus');
+let cu = document.getElementById('contactus');
+let ls = document.getElementById('ls');
+let back=document.getElementById('back');
+home.addEventListener('click',()=>{
+    window.location.href='pathshala.html';
+})
+au.addEventListener('click',()=>{
+    window.location.href='aboutus.html';
+})
+cu.addEventListener('click',()=>{
+    window.location.href='contactus.html';
+})
+ls.addEventListener('click',()=>{
+    window.location.href='login.html';
+})
+back.addEventListener('click',()=>{
+    window.location.href='pathshala.html';
+})
